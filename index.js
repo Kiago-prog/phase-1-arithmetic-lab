635e64a870f27df47dacd03071385b21c09ddd0a
Kiago-prog <roy.kiago@student.moringaschool.com>
2025-03-04 12:20:52 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()+1)
+
+const num3=84
+const num4=20
+const mod=num3 % num4
+
+const numbers = [1, 5, 10, 15, 20]
+const max = Math.max(numbers[4])
